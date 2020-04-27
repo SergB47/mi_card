f6d967bf5ef76f5061b0534176d1daf09120f1db
Sergey Bublikov <bublikovsv47@gmail.com>
2020-04-28 01:51:27 +0300

    single - child container (text)

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,16 @@ class MyApp extends StatelessWidget {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal[700],
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.all(20.0),
+            padding: EdgeInsets.all(20.0),
+            color: Colors.white,
+            child: Text('Hello'), 
+          ),
+        ),
       ),
     );
   
