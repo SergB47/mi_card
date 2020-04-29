7b4e49ba9d671534d8788a0190ed2d0634d4c64c
Sergey Bublikov <bublikovsv47@gmail.com>
2020-04-30 02:09:36 +0300

    add container for contact info

--- lib/main.dart
+++ lib/main.dart
@@ -29,12 +29,53 @@ class MyApp extends StatelessWidget {
                   fontSize: 40.0
                   )),
                 ),
-                Text('Flutter Developer',
+              Text(
+                'Flutter Developer',
                 style: GoogleFonts.abrilFatface(textStyle: TextStyle(
                   color: Colors.teal.shade200,
                   fontSize: 20.0
                 )),
-                )
+              ), 
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.symmetric(vertical:10, horizontal: 25),
+                child: Row(children: <Widget>[
+                  Icon(
+                    Icons.phone,
+                    color: Colors.teal.shade400,
+                  ),
+                  SizedBox( 
+                    width: 10,
+                  ),
+                  Text(
+                    '+7 111 333 22 22',
+                    style: GoogleFonts.abrilFatface(textStyle: TextStyle(
+                      color: Colors.teal.shade400
+                    )),
+                  )
+                ],),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.symmetric(vertical:10, horizontal: 25),
+                child: Row(children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    color: Colors.teal.shade400,
+                  ),
+                  SizedBox( 
+                    width: 10,
+                  ),
+                  Text(
+                    'lname@email.com',
+                    style: GoogleFonts.abrilFatface(textStyle: TextStyle(
+                      color: Colors.teal.shade400
+                    )),
+                  )
+                ],),
+              )
             ],
           ), 
         ),
